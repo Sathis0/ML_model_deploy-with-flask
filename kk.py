96220260cf9368173e0a808824d9226623b0c505
Sathish K <117091624+Sathis0@users.noreply.github.com>
2023-06-23 12:00:12 +0530

    Add files via upload

---
+++ kk.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+app = Flask(__name__)
+model=pickle.load(open("C:/Users/kanna/model.pkl","rb"))
+@app.route('/')
+def home():
+    return render_template('result.html')
+
+def ValuePredictor(to_predict_list):
+	to_predict = np.array(to_predict_list).reshape(1, 12)
+	result =model.predict(to_predict)
+	return result[0]
+
+@app.route('/result', methods = ['POST'])
+def result():
+	if request.method == 'POST':
+		to_predict_list = request.form.to_dict()
+		to_predict_list = list(to_predict_list.values())
+		to_predict_list = list(map(int, to_predict_list))
+		result = ValuePredictor(to_predict_list)	
+		if int(result)== 1:
+			prediction ='Income more than 50K'
+		else:
+			prediction ='Income less that 50K'		
+		return render_template("result.html", prediction = prediction)
+if __name__ == '__main__':
+	app.run()
